f6808d8a7145b868ffc668b556e6d60961022144
Melvin Gilbert <mlvglb@gmail.com>
2021-04-22 13:26:45 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -40,7 +40,10 @@ $config['assets'] = $config['base_url'].'assets/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$base_url = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+$base_url .= "://". @$_SERVER['HTTP_HOST'];
+$base_url .=     str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+$config['base_url'] = $base_url;
 
 /*
 |--------------------------------------------------------------------------
